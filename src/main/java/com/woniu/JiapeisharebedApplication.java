52f40176d1cc996ae5260c2099dc58676cf49b55
sdiq0707 <1255727917@qq.com>
2019-08-19 14:56:52 +0800

    搭建环境

---
+++ src/main/java/com/woniu/JiapeisharebedApplication.java
@@ -0,0 +1,13 @@
+package com.woniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiapeisharebedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiapeisharebedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woniu/JiapeisharebedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woniu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JiapeisharebedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
