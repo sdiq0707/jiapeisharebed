01eebf87d253f28a89d2c9542e6912dec5867bc3
hao <15509186956@163.com>
2019-08-23 22:22:18 +0800

    hsc added trouble crud

---
+++ src/main/java/com/woniu/service/ITroubleService.java
@@ -0,0 +1,19 @@
+package com.woniu.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.data.domain.Page;
+
+import com.woniu.entity.Bed;
+import com.woniu.entity.PageBean;
+import com.woniu.entity.Trouble;
+
+public interface ITroubleService {
+	public List findAll(PageBean pb);
+	public void save(Trouble trouble);
+	public void delete(Integer tid);
+	public void revoke(Integer tid);
+	public Trouble findById(Integer tid);
+	public void update(Trouble trouble);
+}
